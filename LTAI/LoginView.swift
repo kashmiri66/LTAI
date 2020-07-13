3e597d1f8805728fbac48bc17cb25ff08f0db29f
Atta Kashmiri <akkashmi@amazon.com>
2020-07-13 01:44:11 -0700

    ayyyy

---
+++ LTAI/LoginView.swift
@@ -0,0 +1,76 @@
+//
+//  LoginView.swift
+//  LTAI
+//
+//  Created by Kashmiri, Atta on 7/13/20.
+//  Copyright © 2020 Battabar. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    // MARK: - Propertiers
+    @State private var email = ""
+    @State private var password = ""
+    
+    // MARK: - View
+    var body: some View {
+        VStack() {
+            Text("Let's Talk About It")
+                .font(.largeTitle).foregroundColor(Color.white)
+                .padding([.top, .bottom], 40)
+                .shadow(radius: 10.0, x: 20, y: 10)
+            
+            VStack(alignment: .leading, spacing: 15) {
+                TextField("Email", text: self.$email)
+                    .padding()
+                    .background(Color.themeTextField)
+                    .cornerRadius(20.0)
+                    .shadow(radius: 10.0, x: 20, y: 10)
+                
+                SecureField("Password", text: self.$password)
+                    .padding()
+                    .background(Color.themeTextField)
+                    .cornerRadius(20.0)
+                    .shadow(radius: 10.0, x: 20, y: 10)
+            }.padding([.leading, .trailing], 27.5)
+            
+            Button(action: {}) {
+                Text("Sign In")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(width: 300, height: 50)
+                    .background(Color.green)
+                    .cornerRadius(15.0)
+                    .shadow(radius: 10.0, x: 20, y: 10)
+            }.padding(.top, 50)
+            
+            Spacer()
+            HStack(spacing: 0) {
+                Text("Don't have an account? ")
+                Button(action: {}) {
+                    Text("Sign Up")
+                        .foregroundColor(.black)
+                }
+            }
+        }
+        .navigationBarBackButtonHidden(true)
+        .background(
+            LinearGradient(gradient: Gradient(colors: [.purple, .blue]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all))
+        
+    }
+}
+
+extension Color {
+    static var themeTextField: Color {
+        return Color(red: 220.0/255.0, green: 230.0/255.0, blue: 230.0/255.0, opacity: 1.0)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
